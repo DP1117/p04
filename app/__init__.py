33ee8daa32a79befdb98ada4c45f1ccb124ec45c
Aditya Anand <aanand50@stuy.edu>
2025-04-01 11:35:29 -0400

    app

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")                    
-def hello_world():
-    print(__name__)                  
-  
+def hello():
+    return("hello")
 app.run()                                
