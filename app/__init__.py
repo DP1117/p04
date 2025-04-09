319de0d6499d492a8dd626bc30a66e25f2e22664
PureJZ <jzeng246@gmail.com>
2025-04-09 11:13:46 -0400

    delete not needed imports

--- app/__init__.py
+++ app/__init__.py
@@ -7,13 +7,13 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-from plotly.offline import init_notebook_mode
+#from plotly.offline import init_notebook_mode
 import seaborn as sns
 import datetime as dt
 import warnings
 warnings.filterwarnings('ignore')
 pd.set_option('display.max_columns',None)
-init_notebook_mode(connected=True)
+#init_notebook_mode(connected=True)
 
 df=pd.read_csv('songs_normalize.csv')
 
@@ -21,7 +21,7 @@ df=pd.read_csv('songs_normalize.csv')
 
 
 app = Flask(__name__)
-app.secret_key = 'supersecretkey'  
+app.secret_key = 'supersecretkey'
 
 DB_PATH = 'users.db'
 
